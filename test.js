65987909276b31ffb8bc4cd3f6917e7649065e47
POS Scalev Developer <developer@posscalev.local>
2026-07-30 18:33:18 +0700

    chore: cleanup unused files

--- test.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const w = await prisma.warehouses.findFirst();
-  console.log(w);
-}
-main();
